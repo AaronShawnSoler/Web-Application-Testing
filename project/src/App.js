a0f4795ba59cfbb21d00e48ee25107da15ba5f82
Aaron Shawn Soler <aaronshawnsoler@gmail.com>
2019-12-05 13:32:57 -0500

    project setup

---
+++ project/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+import Display from './components/Display';
+import Dashboard from './components/Dashboard';
+
+function App() {
+
+  const [strikes, setStrikes] = useState(0);
+  const [balls, setBalls] = useState(0);
+
+  return (
+    <div className="App">
+      <Display strikes={strikes} balls={balls} />
+      <Dashboard strikes={strikes} balls={balls} setStrikes={setStrikes} setBalls={setBalls} />
+    </div>
+  );
+}
+
+export default App;
